1cb7ea7e7a91102f1023feb318479e4c204506cf
vladimir <lovkiy2012@gmail.com>
2020-07-29 17:42:45 +0300

    15 connection utilities

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const orderRoutes = require('./routes/order');
 
 const app = express();
 
-//   app.use(bodyParser.urlencoded({extended:true})
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(require('cors')());
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
