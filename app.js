3ed28b8b59e67d46768147cbcdbf198c5f414b26
vladimir <lovkiy2012@gmail.com>
2020-07-29 19:30:54 +0300

    18 connected mongoDB

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const positionRoutes = require('./routes/position');
 const orderRoutes = require('./routes/order');
+const keys = require('./config/keys');
 
 const app = express();
+
+mongoose
+    .connect(keys.mongoURI)
+    .then(() => console.log('MongoDB connected...'))
+    .catch((error) => console.log(error));
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+        'mongodb+srv://vladimir:OdbAmIK6Ac9sLkMf@cluster0.nsjhu.mongodb.net/vladimir?retryWrites=true&w=majority',
+};
