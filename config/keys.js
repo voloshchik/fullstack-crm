dc98a4c41d8f58853dd27ceea2dcc09bc6f0f9a4
vladimir <lovkiy2012@gmail.com>
2020-07-29 21:47:42 +0300

    23

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI:
         'mongodb+srv://vladimir:OdbAmIK6Ac9sLkMf@cluster0.nsjhu.mongodb.net/vladimir?retryWrites=true&w=majority',
+    jwt: 'dev-jwt',
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,41 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys');
 
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password,
-        },
+module.exports.login = async function (req, res) {
+    const canditate = await User.findOne({
+        email: req.body.email,
     });
+
+    if (canditate) {
+        const passwordResult = bcrypt.compareSync(
+            req.body.password,
+            canditate.password
+        );
+        if (passwordResult) {
+            const token = jwt.sign(
+                {
+                    email: canditate.email,
+                    userId: canditate._id,
+                },
+                keys.jwt,
+                { expiresIn: 60 * 60 }
+            );
+
+            res.status(200).json({
+                token: `Bearer ${token}`,
+            });
+        } else {
+            res.status(401).json({
+                message: 'Пароли не совпадают',
+            });
+        }
+    } else {
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден!',
+        });
+    }
 };
 
 module.exports.register = async function (req, res) {
