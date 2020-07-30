9e8a3f45c0b17241ad81319c4245cb385c3550ec
vladimir <lovkiy2012@gmail.com>
2020-07-30 21:48:44 +0300

    27 getting all positions

--- controllers/position.js
+++ controllers/position.js
@@ -1,4 +1,35 @@
-module.exports.getByCategoryId = function (req, res) {};
-module.exports.create = function (req, res) {};
-module.exports.remove = function (req, res) {};
-module.exports.update = function (req, res) {};
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id,
+        });
+        res.status(200).json(positions);
+    } catch (e) {
+        errorHandler(res, e);
+    }
+};
+
+module.exports.create = async function (req, res) {
+    try {
+    } catch (e) {
+        errorHandler(res, e);
+    }
+};
+
+module.exports.remove = async function (req, res) {
+    try {
+    } catch (e) {
+        errorHandler(res, e);
+    }
+};
+
+module.exports.update = async function (req, res) {
+    try {
+    } catch (e) {
+        errorHandler(res, e);
+    }
+};
