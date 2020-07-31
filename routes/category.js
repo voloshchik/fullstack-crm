4d69b2e901e840e2cb474b7fd43cccabe42e9e3a
vladimir <lovkiy2012@gmail.com>
2020-07-31 20:27:49 +0300

    31 creating catrgories

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const passport = require('passport');
+
+const upload = require('../middleware/upload');
 const router = express.Router();
 const controller = require('../controllers/category');
 
@@ -9,9 +11,23 @@ router.get(
     controller.getAll
 );
 
-router.get('/:id', controller.getById);
+router.get(
+    '/:id',
+    passport.authenticate('jwt', { session: false }),
+    controller.getById
+);
 router.delete('/:id', controller.remove);
-router.post('/', controller.create);
-router.patch('/:id', controller.update);
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    upload.single('image'),
+    controller.create
+);
+router.patch(
+    '/:id',
+    passport.authenticate('jwt', { session: false }),
+    upload.single('image'),
+    controller.update
+);
 
 module.exports = router;
