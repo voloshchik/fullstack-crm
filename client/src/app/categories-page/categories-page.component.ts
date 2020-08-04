882823998b299d100a5300ba68789fd055dc9420
vladimir <lovkiy2012@gmail.com>
2020-08-04 19:58:30 +0300

    51 page categories

--- client/src/app/categories-page/categories-page.component.ts
+++ client/src/app/categories-page/categories-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../shared/services/categories.service';
+import { Category } from '../shared/interfaces';
 
 @Component({
   selector: 'app-categories-page',
   templateUrl: './categories-page.component.html',
-  styleUrls: ['./categories-page.component.scss']
+  styleUrls: ['./categories-page.component.scss'],
 })
 export class CategoriesPageComponent implements OnInit {
-
-  constructor() { }
+  loading = false;
+  categories: Category[] = [];
+  constructor(private categoriesService: CategoriesService) {}
 
   ngOnInit(): void {
+    this.loading = true;
+    this.categoriesService.fetch().subscribe((categories) => {
+      this.loading = false;
+      this.categories = categories;
+    });
   }
-
 }
--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -2,3 +2,10 @@ export interface User {
   email: String;
   password: String;
 }
+
+export interface Category {
+  name: string;
+  imageSrc?: string;
+  user?: string;
+  _id?: string;
+}
